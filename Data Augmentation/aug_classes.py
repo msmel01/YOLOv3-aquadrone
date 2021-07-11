c08dfe922c5ffa1284a7c61fb2762d8430f04c1d
msmel01 <ms.elly17.01@gmail.com>
2021-07-11 12:12:25 -0400

    add setDefault method to aug classes

--- Data Augmentation/aug_classes.py
+++ Data Augmentation/aug_classes.py
@@ -1,51 +1,88 @@
 class HorizontalFlipParams():
-    p = 1.0
+    def __init__(self):
+        # self.p = 1.0
+        self.setDefault()
+
+
+    def setDefault(self):
+        self.p = 1.0
 
 
 class MotionBlurParams():
-    always_apply = False
-    p = 1.0
-    blur_limit = (3, 7) # 3 - 100
+    def __init__(self):
+        self.setDefault()
+
+
+    def setDefault(self):
+        self.always_apply = False
+        self.p = 1.0
+        self.blur_limit = [3, 7] # 3 - 100
 
 
 class IsoNoiseParams():
-    always_apply = False
-    p = 1.0
-    intensity = (0.1, 0.5) # 0 - 1
-    color_shift = (0.01, 0.05) # 0 - 1
+    def __init__(self):
+        self.setDefault()
+
+
+    def setDefault(self):
+        self.always_apply = False
+        self.p = 1.0
+        self.intensity = [0.1, 0.5] # 0 - 1
+        self.color_shift = [0.01, 0.05] # 0 - 1
 
 
 class RotateParams():
-    always_apply = False
-    p = 1.0
-    limit = (-90, 90)
-    interpolation = 0 # can be from 0 to 4
-    border_mode = 0 # can be from 0 to 4
-    value = (0, 0, 0) # 0 - 255 rgb
-    mask_value = None
+    def __init__(self):
+        self.setDefault()
+
+
+    def setDefault(self):
+        self.always_apply = False
+        self.p = 1.0
+        self.limit = [-90, 90]
+        self.interpolation = 0 # can be from 0 to 4
+        self.border_mode = 0 # can be from 0 to 4
+        self.value = [0, 0, 0] # 0 - 255 rgb
+        self.mask_value = None
+
 
 
 class CutOutParams():
-    always_apply = False
-    p = 1.0
-    num_holes = 8 # 1 - 100
-    max_h_size = 8 # 1 - 100
-    max_w_size = 8 # 1 - 100
+    def __init__(self):
+        self.setDefault()
+
+
+    def setDefault(self):
+        self.always_apply = False
+        self.p = 1.0
+        self.num_holes = 8 # 1 - 100
+        self.max_h_size = 8 # 1 - 100
+        self.max_w_size = 8 # 1 - 100
 
 
 class CropParams():
-    always_apply = False
-    p = 1.0
-    x_min = 0 # 0 - 320 for xmin and xmax
-    y_min = 0 # 0 - 213 for ymin and ymax
-    x_max = 160 
-    y_max = 106
+    def __init__(self):
+        self.setDefault()
+
+
+    def setDefault(self):
+        self.always_apply = False
+        self.p = 1.0
+        self.x_min = 0
+        self.y_min = 0
+        self.x_max = 160 
+        self.y_max = 106
 
 
 class RgbShiftParams():
-    always_apply = False
-    p = 1.0
-    # can be from -255 to 255
-    r_shift_limit = (-20, 20)
-    g_shift_limit = (-20, 20)
-    b_shift_limit = (-20, 20)
+    def __init__(self):
+        self.setDefault()
+
+
+    def setDefault(self):
+        self.always_apply = False
+        self.p = 1.0
+        # can be from -255 to 255
+        self.r_shift_limit = [-20, 20]
+        self.g_shift_limit = [-20, 20]
+        self.b_shift_limit = [-20, 20]
